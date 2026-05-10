07b2f7b64bc0eae321e152b7f67cccc188e01fae
kveze <zinovievvlad4@gmail.com>
2026-05-10 11:42:41 +0300

    kokL

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Generate from './pages/Generate'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import PrivateRoute from './components/PrivateRoute'
-import Today from './pages/Today'
+
 import Profile from './pages/Profile'
 import LooksMaxTip from './pages/LooksMaxTip'
 import Chat from './pages/Chat'
@@ -17,7 +17,6 @@ export default function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/looksmax" element={<PrivateRoute><LooksMaxTip /></PrivateRoute>} />
-      <Route path="/today" element={<PrivateRoute><Today /></PrivateRoute>} />
       <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
       <Route path="/chat" element={<Chat />} />
     </Routes>
